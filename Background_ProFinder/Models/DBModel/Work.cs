9b665f4c82e559e8598f06fd854de74dc3ed6196
Chi-Ling-Lee <19940818jillian@gmail.com>
2021-09-14 21:49:20 +0800

    fproject initialization

---
+++ Background_ProFinder/Models/DBModel/Work.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Background_ProFinder.Models.DBModel
+{
+    public partial class Work
+    {
+        public Work()
+        {
+            WorkPictures = new HashSet<WorkPicture>();
+        }
+
+        public int WorkId { get; set; }
+        public string WorkName { get; set; }
+        public string WorkDescription { get; set; }
+        public string Client { get; set; }
+        public string Role { get; set; }
+        public int YearStarted { get; set; }
+        public string WebsiteUrl { get; set; }
+        public int SubCategoryId { get; set; }
+        public int? WorkAttachmentId { get; set; }
+        public int? MemberId { get; set; }
+
+        public virtual SubCategory SubCategory { get; set; }
+        public virtual WorkAttachment WorkAttachment { get; set; }
+        public virtual ICollection<WorkPicture> WorkPictures { get; set; }
+    }
+}
