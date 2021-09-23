0876b2c700d44245b611b3754b4a7f655e5861ce
sandydai05 <ac556679@yahoo.com.tw>
2021-09-23 22:48:05 +0800

    commit

--- Background_ProFinder/APIControllers/LoginApiController.cs
+++ Background_ProFinder/APIControllers/LoginApiController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Background_ProFinder.Models.ViewModel;
 using Background_ProFinder.Service;
 using System.Web;
+using Background_ProFinder.Models.APIModel;
 
 namespace Background_ProFinder.APIControllers
 {
@@ -28,7 +29,7 @@ namespace Background_ProFinder.APIControllers
         {
             try
             {
-               var result = _loginService.GetUserAccountData();
+                var result = _loginService.GetUserAccountData();
                 return new APIResult(APIStatus.Success, string.Empty, result);
             }
             catch (Exception ex)
@@ -52,30 +53,9 @@ namespace Background_ProFinder.APIControllers
                 return new APIResult(APIStatus.Fail, ex.Message, result);
             }
 
-            
-        }
-
-        public class APIResult
-        {
-            public APIResult(int status, string errMsg, object result)
-            {
-                Status = status;
-                ErrMsg = errMsg;
-                Result = result;
-            }
-            public int Status { get; set; }
-            public string ErrMsg { get; set; }
-            public object Result { get; set; }
 
         }
 
-        public static class APIStatus
-        {
-            public const int Success = 0;
-            public const int Fail = 1;
-            public const int DataBaseBreak = 101;
-
-        }
 
     }
 }
